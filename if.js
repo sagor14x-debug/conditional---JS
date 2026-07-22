ef4720cea9f2bcbaad115a52ad55e44ad2f0823c
sagor14x-debug <sagor14x@gmail.com>
2026-07-22 19:51:57 +0600

    Add if.js

--- if.js
+++ if.js
@@ -1,54 +1,54 @@
-// if (false) {
-// console.log("inside the condition");
-// }
-// if (false) {
-// console.log("inside the condition");
-// }
+if (false) {
+console.log("inside the condition");
+}
+if (false) {
+console.log("inside the condition");
+}
 
-// if ( 8 < 5) {
-//     console.log("inside the condition");
-// }
-// if ( 8 > 5) {
-//     console.log("inside the condition");
-//     console.log("8 is bigger than 3");
-// }
+if ( 8 < 5) {
+    console.log("inside the condition");
+}
+if ( 8 > 5) {
+    console.log("inside the condition");
+    console.log("8 is bigger than 3");
+}
 
-// if ( 50 <= 45) {
-//     console.log("Coming inside the condition");
-// }
+if ( 50 <= 45) {
+    console.log("Coming inside the condition");
+}
 
-// if ( 50 <= 50) {
-//     console.log("Coming inside the condition");
-// }
+if ( 50 <= 50) {
+    console.log("Coming inside the condition");
+}
 
-// const price = 400;
-// if (price < 500) {
-//     console.log("price is lower");
-// }
-// const price = 400;
-// if (price > 500) {
-//     console.log("price is lower");
-// }
+const price = 400;
+if (price < 500) {
+    console.log("price is lower");
+}
+const price = 400;
+if (price > 500) {
+    console.log("price is lower");
+}
 
-// let price = 1400;
-// let money = 500;
-// if (price < money) {
-//   console.log("I will buy it");
-// }
+let price = 1400;
+let money = 500;
+if (price < money) {
+  console.log("I will buy it");
+}
 
 
-// let price = 400;
-// let money = 500;
+let price = 400;
+let money = 500;
 
-// if (price > money) {
-//   console.log("I will buy it");
-// }
+if (price > money) {
+  console.log("I will buy it");
+}
 
-// let name = 'Alu';
+let name = 'Alu';
 
-// if (name === 'alu') {
-//     console.log("We are potatho");
-// }
+if (name === 'alu') {
+    console.log("We are potatho");
+}
 
 const isGhorib = true;
 if (isGhorib === true) {
